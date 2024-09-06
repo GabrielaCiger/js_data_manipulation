49c60212a23b9bc912f9654d370f7dc8bcd5af6e
Gabriela CÍGEROVÁ <gabriela.cigerova@Digital-Grenoble.local>
2024-09-06 11:51:46 +0200

    Finished exercise 1

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log(`${cities.length} communes chargées`);
+
+// const cities = [
+//     {
+//         "code":"01001",
+//         "nom":"L'Abergement-Clémenciat",
+//         "population":767,
+//         "codesPostaux":["01400"],
+//         "codeDepartement":"01"
+//     },
+
+let arr = cities.filter((city) => city.population >= 300000)
+/* ? filter() */
+/* For each city in the cities array of objects, check the city.population and if true
+add it to the arr filtered array */
+
+arr.sort((a, b) => b.nom[0].localeCompare(a.nom[0]));
+/* * localeCompare */
+/* localeCompare() compares two strings according to the locale and returns a numeric value
+indicating the relationship between the strings */
+
+arr.forEach(city => console.log(city.nom, city.population));
